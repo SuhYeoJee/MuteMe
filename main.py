07b7d635009e214aaed1aced8f4c9b9477432813
SuhYeoJee <72551449+SuhYeoJee@users.noreply.github.com>
2024-12-26 22:55:43 +0900

    token config

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import discord
 from discord.ext import commands
-
+import configparser
+# -------------------------------------------------------------------------------------------
 # 봇 객체 생성
 intents = discord.Intents.default()
 intents.message_content = True  # 메시지 내용 읽기 권한
 bot = commands.Bot(command_prefix="!", intents=intents)
+# -------------------------------------------------------------------------------------------
+config = configparser.ConfigParser()
+config.read('config.ini')
+# ===========================================================================================
 
 # 봇이 준비되었을 때
 @bot.event
 async def on_ready():
     print(f"{bot.user} has connected to Discord!")
 
-
+# -------------------------------------------------------------------------------------------
 @bot.command(name='hi')
 async def hello(ctx):
     await ctx.send('Hello world!')
 
-
 # ===========================================================================================
-# 봇 토큰을 사용하여 실행
-bot.run('여기에 봇 토큰을 입력')
+bot.run(config['MuteMe']['token'])
